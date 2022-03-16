1061e2d86407425954147304483be51a10abd644
huntekah <krzysztof.jurkiewicz@stxnext.com>
2022-03-16 11:38:12 +0100

    feature: script for translating tweets

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # prezentacja_STX_ON_TOUR_03.2022
+
+1. First create an account on Twitter, and register as a developer.
+2. Use [crawler](get_tweets.py) to download tweets according to [topics](topics.py)
+   1. You might need to filter retweets and similar tweets with a [simple script](filter_repetitive_tweets.sh).
+3. [Translate](translate_tweets.py) your tweets.
+4. [TODO]
--- translate_tweets.py
+++ translate_tweets.py
@@ -1,13 +1,62 @@
 from transformers import pipeline
 from pathlib import Path
-from typing import Tuple
+from typing import Tuple, Dict, Optional
 from get_tweets import TWEETS_LOCATION
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
+import logging
 
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(funcName)s - %(message)s ",
+    datefmt="%H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+TRANSLATIONS_LOCATION = Path("./translations")
 model_checkpoints = {
     "ru_en": "Helsinki-NLP/opus-mt-ru-en",
     "pl_en": "Helsinki-NLP/opus-mt-pl-en",
     "uk_en": "Helsinki-NLP/opus-mt-uk-en",
 }
+
+translators: Dict[str, pipeline] = {}
+
+
+def translate_file(lang: str, file_: Path, progress_bar: Optional[tqdm] = None) -> None:
+    translator = get_translator(lang)
+    result_file = TRANSLATIONS_LOCATION / file_.name
+    with file_.open() as fh, result_file.open("w") as rfh:
+        while line := fh.readline():
+            translation_raw = translator(line)
+            try:
+                translation = translation_raw[0]["translation_text"]
+            except:
+                continue
+            if progress_bar:
+                progress_bar.update()
+
+            logger.info(
+                f"Translated sentence:\n\t{line.strip()}\n\t{translation.strip()}"
+            )
+            rfh.write(translation.strip() + "\n")
+
+
+def get_translator(lang: str) -> pipeline:
+    key = f"{lang}_en"
+    if translator := translators.get(key):
+        return translator
+    else:
+        translators[key] = pipeline("translation", model=model_checkpoints[key])
+        return translators[key]
+
+
+def get_lines_count(location: Path) -> int:
+    result = 0
+    for lang in ("ru", "pl", "uk"):
+        for tweet_collection in TWEETS_LOCATION.glob(f"{lang}_*.tsv"):
+            result += sum(1 for line in tweet_collection.open())
+    return result
 
 
 def iter_tweet_collections(location: Path) -> Tuple[str, Path]:
@@ -16,16 +65,10 @@ def iter_tweet_collections(location: Path) -> Tuple[str, Path]:
             yield lang, tweet_collection
 
 
-def translate_file(lang: str, file_: Path) -> None:
-    with file_.open() as fh:
-        while line := fh.readline():
-            print(file_.name, line)
-
-
 if __name__ == "__main__":
-    for lang, tweet_collection in iter_tweet_collections(TWEETS_LOCATION):
-        translate_file(lang, tweet_collection)
-        # print(tweets_topic_collection)
-        # print(type(tweets_topic_collection))
-        # print(dir(tweets_topic_collection))
-        # exit()
+    with logging_redirect_tqdm(), tqdm(
+        total=get_lines_count(TWEETS_LOCATION)
+    ) as progress_bar:
+        for lang, tweet_collection in iter_tweet_collections(TWEETS_LOCATION):
+            logger.info(f"Processing {tweet_collection.name}")
+            translate_file(lang, tweet_collection, progress_bar)
